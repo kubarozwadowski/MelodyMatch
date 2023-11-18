31af7a3747dab3ca43bff99a0f65c0737942560d
MarcoDe05 <147563388+MarcoDe05@users.noreply.github.com>
2023-11-18 17:29:48 -0500

    Update Login JS.js

--- Login JS.js
+++ Login JS.js
@@ -1,11 +1,22 @@
-// app.js
 const clientId = 'dd8233fe305a40698c7596d33b5232ef'; // your clientId
+// Use only one of the following lines for redirectUrl
 const redirectUrl = 'https://kubarozwadowski.github.io/MelodyMatch/handleSpotifyResponse.html'; // Updated redirect URL
-const redirectUrl = 'https://kubarozwadowski.github.io/MelodyMatch/'; // your redirect URL - must be localhost URL and/or HTTPS
+// OR
+// const redirectUrl = 'https://kubarozwadowski.github.io/MelodyMatch/'; // your redirect URL - must be localhost URL and/or HTTPS
 
 const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const scope = 'user-read-private user-read-email';
-@@ -17,6 +18,32 @@ function redirectToSpotifyAuthorize() {
+
+function redirectToSpotifyAuthorize() {
+  const authUrl = new URL(authorizationEndpoint);
+  const params = {
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUrl,
+  };
+
+  authUrl.search = new URLSearchParams(params).toString();
   window.location.href = authUrl.toString(); // Redirect the user to the authorization server for login
 }
 
@@ -20,7 +31,8 @@ function handleLoginCallback() {
 
     // After processing, redirect to another page (e.g., index.html)
     window.location.href = 'index.html';
-  }
+}
+
 // After obtaining the authorization code, call handleLoginCallback
 const code = args.get('code');
 if (code) {
